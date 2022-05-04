082f95750243998cee862b3968f9c58c0d051d63
smahm094 <94198655+smahm094@users.noreply.github.com>
2022-05-03 20:28:56 -0400

    Update class_exercise_1.py

--- class_exercise_1.py
+++ class_exercise_1.py
@@ -1,6 +1,21 @@
-x = int(input())
-y = int(input())
-print(x + y)
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print(a + b)
+x1 = input("Number 1:")
+x1 = int(x1)
+x2 = input("Number 2:")
+x2 = int(x2)
+x3 = input("Number 3:")
+x3 = int(x2)
+
+sum = (x1 + x2) + x3
+print(sum)
+
+sub = (x1 - x2) - x3
+print(sub)
+
+mul = (x1*x2)*x3
+print(mul)
+
+div = (x1/x2)/x3
+print(div)
+
+power = (x1**x1)**x3
+print(power)
